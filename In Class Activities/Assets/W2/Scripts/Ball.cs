756e57945732744bfcee87f4f88bdbe0a5fc66c1
brianoz-alt <brian.ornelas777@gmail.com>
2025-10-13 17:36:59 -0700

    W3

--- In Class Activities/Assets/W2/Scripts/Ball.cs
+++ In Class Activities/Assets/W2/Scripts/Ball.cs
@@ -21,7 +21,7 @@ public class Ball : MonoBehaviour
         //_bounces = _bounces + 2;
         //_bounces + 1
         //_bounces --;
-        //_bounces++;
+        _bounces++;
         //_bounces += 1
         // STEP 1 -------------------------------------------------------------
 
@@ -44,7 +44,7 @@ public class Ball : MonoBehaviour
         // Uncomment ONE of the below lines to ADD 0.1 to the value of 'r'.
         //r += 0.1f
         //r * 0.1;
-        //r = r + 0.1f;
+        r = r + 0.1f;
         //r = r * 0.1f;
         //r += 0.1;
         // STEP 2 -------------------------------------------------------------
@@ -57,33 +57,33 @@ public class Ball : MonoBehaviour
             // STEP 3 ---------------------------------------------------------
             // Uncomment ONE of the below lines of code to set the value of 'r' to ZERO.
             //r = 0.0f
-            //r = 0.0f;
+            r = 0.0f;
             //r 0.0f;
             // STEP 3 ---------------------------------------------------------
         }
 
         // STEP 4 -------------------------------------------------------------
         // Uncomment the below line and fix it to SUBTRACT 0.1 from the value of 'g'.
-        //g -= 0.1f
+        g -= 0.1f;
         // STEP 4 -------------------------------------------------------------
 
         if (g < 0.0f)
         {
             // STEP 5 ---------------------------------------------------------
             // Uncomment the below line and fix it to set the value of 'g' to ONE.
-            //g = 1.0;
+            g = 1.0f;
             // STEP 5 ---------------------------------------------------------
         }
 
         // STEP 6 -------------------------------------------------------------
         // Below this comment, write a line of code to MULTIPLY 'b' by 1.2.
-
+        b *= 1.2f;
         // STEP 6 -------------------------------------------------------------
 
         // STEP 7 -------------------------------------------------------------
         // Below this comment, write an IF STATEMENT:
         // IF the value of 'b' is GREATER THAN **OR** EQUAL TO 1.0,
-        //      set 'b' to 0.1. 
+        if (b >= 1.0f) {b = 0.1f;} 
         
         // STEP 7 -------------------------------------------------------------
 
@@ -103,12 +103,13 @@ public class Ball : MonoBehaviour
         // Below this comment, write 2 lines of code to ADD the values of 
         //      r, g, and b, and then divide the result by 3.
         // Store the result in a variable named 'brightness'.
-        
+      float brightness = (r + b + g);
+        brightness = (r + b + g) / 3.0f;
         // STEP 8 -------------------------------------------------------------
 
         // STEP 9 -------------------------------------------------------------
         // Uncomment the below line to make the 'brightness' text change in your game.
-        //_brightnessText.text = "brightness = " + brightness;
+        _brightnessText.text = "brightness = " + brightness;
         // STEP 9 -------------------------------------------------------------
     }
 }
