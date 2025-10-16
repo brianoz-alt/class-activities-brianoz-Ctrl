b67f7fceb318130f47e594eae7791e66656f4cbc
brianoz-alt <brian.ornelas777@gmail.com>
2025-10-16 14:25:33 -0700

    w3

--- In Class Activities/Assets/W3/Scripts/BallW3.cs
+++ In Class Activities/Assets/W3/Scripts/BallW3.cs
@@ -1,12 +1,12 @@
 using TMPro;
 using UnityEngine;
 
-public class BallW3
+public class BallW3 : MonoBehaviour
 {
     public SpriteRenderer ballRenderer;
-    private Rigidbody2D _rigidbody;
-    private float _speedMultiplier = 1.0f;
-    private float _speedThreshold = 10.0f;
+    [SerializeField]private Rigidbody2D _rigidbody;
+    [SerializeField]private float _speedMultiplier = 1.0f;
+    [SerializeField] private float _speedThreshold = 10.0f;
 
     // ------------------------------------------------------------------------
     // This method is called by Unity whenever the ball hits something.
@@ -24,7 +24,7 @@ public class BallW3
         //      of _speedMultiplier. TUNE that value in the Inspector and see
         //      what happens!
 
-        //rigidbody.linearvelocity *= speedmultiplier;
+        _rigidbody.linearVelocity =_rigidbody.linearVelocity*_speedMultiplier;
 
         // STEP 1 -------------------------------------------------------------
 
--- In Class Activities/Assets/W3/Scripts/CatW3.cs
+++ In Class Activities/Assets/W3/Scripts/CatW3.cs
@@ -95,8 +95,8 @@ public class CatW3 : MonoBehaviour
             // Below this comment, CALL the method named DecreaseHealth.
             // Notice this method's return type is void- that means we don't
             //      have to store the result anywhere.
-            
-
+            //
+           DecreaseHealth();
             // STEP 2 ---------------------------------------------------------
 
             // STEP 6 ---------------------------------------------------------
@@ -107,7 +107,7 @@ public class CatW3 : MonoBehaviour
             //
             // Try toggling the Destroy Cat When Dead setting on the Inspector,
             //      and see how the cat is removed ONLY when it's checked!
-            
+
 
             // STEP 6 ---------------------------------------------------------
         }
@@ -129,8 +129,8 @@ public class CatW3 : MonoBehaviour
     private void DecreaseHealth()
     {
         // write Step 3 below this comment!
-
-
+        _health = _health - 1;
+        _healthText.text = "health =" + _health;
         // STEP 5 -------------------------------------------------------------
         // Once you've finished Step 4, CALL the GetHealthSpeechText method
         //      and store the result in _speechText's text variable.
